ac4bc55fd8f4d24155934ad20d35b1429454dbe4
agra <agramatik@spotoption.com>
2018-12-25 12:29:19 +0200

    added enzyme + dependencies

--- src/__tests__/components/Header.test.js
+++ src/__tests__/components/Header.test.js
@@ -1,9 +1,8 @@
 import React from 'react'
-import ReactShallowRenderer from 'react-test-renderer/shallow'
+import { shallow } from 'enzyme';
 import Header from '../../components/Header';
 
 test('renderHeaderComponet', () => {
-    const render = new ReactShallowRenderer();
-    render.render(<Header/>);
-    expect(render.getRenderOutput()).toMatchSnapshot();
+    const wrapper = shallow(<Header/>)
+    expect(wrapper).toMatchSnapshot();
 })
