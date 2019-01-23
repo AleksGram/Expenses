22064f569706f212698c09fed7334d5166bb39e9
AlexGram <agramatik1@gmail.com>
2019-01-23 23:22:07 +0200

    added total expenses selector + test

--- src/__tests__/selectors/expenses-total.test.js
+++ src/__tests__/selectors/expenses-total.test.js
@@ -1,28 +1,19 @@
 import selectExpensesTotal from '../../selectors/expenses-total';
 import expenses from '../../testFixtures/expenses';
 
-const getExpensesTottal = (expenses) => {
-    if(expenses && expenses.length > 0) {
-        return expenses.reduce((accumulator, expense)=>{
-           return accumulator + expense.amount
-        },0)    
-    } else {
-        return 0;
-    }
-};
 
 test('total_noExpenses', () => {
-    const total = getExpensesTottal();
+    const total = selectExpensesTotal([]);
     expect(total).toBe(0)
 })
 
 test('total_singleExpense', () => {
     const testExpense = [expenses[0]]
-    const total = getExpensesTottal(testExpense)
+    const total = selectExpensesTotal(testExpense)
     expect(total).toBe(expenses[0].amount);
 })
 
 test('total_expenses', () => {
-    const total = getExpensesTottal(expenses);
+    const total = selectExpensesTotal(expenses);
     expect(total).toBe(5150);
 })
