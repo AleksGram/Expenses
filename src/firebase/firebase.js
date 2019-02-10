9f7a2e0faac884bc0ed9311b19d84ea4173e5b9f
AlexGram <agramatik1@gmail.com>
2019-02-11 00:38:13 +0200

    data handling in firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -14,16 +14,43 @@ const config = {
 
   const database = firebase.database()
 
-  database.ref().set({
-      name: 'AlexGram',
-      age: 32,
-      isSingle: false,
-      location: {
-          city: 'Odessa',
-          country: "UA"
-      },
-      workDays: [2,6,8,9]
+  database.ref()
+        .once('value')
+        .then((data) => {
+            console.log(data.val())
+        }).catch((e) => {
+            console.log('Failed', e);
+        });
+     
+        database.ref().on('value', (snapshot) => {
+            const data = snapshot.val();
+            console.log(`${data.name} from ${data.location.city} he is ${data.personData.height} cm `)
+        }, (e) => {
+            console.log('Failed fetching data', e);
+        })
 
-  })
+//   database.ref().set({
+//       name: 'AlexGram',
+//       age: 32,
+//       isSingle: false,
+//       location: {
+//           city: 'Odessa',
+//           country: "UA"
+//       },
+//       workDays: [2,6,8,9]
 
-  database.ref('personData').set({height: 180, weight: 75});
+//   }).then(() => {
+//       console.log('Data is saved');
+//   }).catch((e) => {
+//       console.log('Failed', e);
+//   })
+
+//   database.ref('personData').set({height: 180, weight: 75});
+//   database.ref().update({
+//       age: 30,
+//       'location/city': 'Kyiv'
+//   }).then(() => {
+//     console.log('Data is updated');
+// }).catch((e) => {
+//     console.log('Failed update', e);
+// })
