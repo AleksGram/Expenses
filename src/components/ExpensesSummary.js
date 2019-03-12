0a38e279c5fce48c1d06ff442d60c41c29745e49
agra <agramatik@spotoption.com>
2019-03-12 12:11:07 +0200

    add total expenses info

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -7,14 +7,18 @@ import numeral from 'numeral';
 
 
 
-export const ExpensesSummary = ({ expenseCount, expenseSummary }) => {
+export const ExpensesSummary = ({ expenseCount, expenseSummary, expenses }) => {
     const description = expenseCount === 1 ? 'expense' : 'expenses';
     const formatTotalVal = numeral(expenseSummary / 100).format('$0,0.00');
+    const difExpenses = expenses.length - expenseCount;
+
+    const totalExpenses = (difExpenses > 0) ? `Expenses out of the filter: ${difExpenses}` : null;
 
     return (
         <div className="page-header">
             <div className="content-container">
                 <h1 className="page-header__title">Viewing <span>{expenseCount}</span> {description} totaling <span>{formatTotalVal}</span> </h1>
+                <h2 className="page-header__title">{totalExpenses}</h2>
                 <div className="page-header__actions">
                     <Link className="button" to="/create" >Add Expense</Link>
                 </div>
@@ -25,10 +29,12 @@ export const ExpensesSummary = ({ expenseCount, expenseSummary }) => {
 
 const mapStateToProps = (state) => {
     const visibleExpenses = selectExpenses(state.expenses, state.filters)
+    console.log(state.expenses);
 
     return {
         expenseCount: visibleExpenses.length,
-        expenseSummary: selectTotalExpenses(visibleExpenses)
+        expenseSummary: selectTotalExpenses(visibleExpenses),
+        expenses: state.expenses
     }
 };
 
